024d73434d3f54ea02df05c36529f8831ea2129a
JAVIERERNESTO <jelh87@gmail.com>
2024-01-24 16:59:56 -0600

    Se suben clases test Mockito

--- src/main/java/com/marvel/microservicio/repository/BitacoraRepository.java
+++ src/main/java/com/marvel/microservicio/repository/BitacoraRepository.java
@@ -1,13 +1,11 @@
 package com.marvel.microservicio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.marvel.microservicio.model.Bitacora;
 
 /**
  * Repositorio
  */
-@Repository
 public interface BitacoraRepository extends JpaRepository<Bitacora,Long>{
 
 }
---
+++ src/test/java/com/marvel/microservicio/microservicio/service/impl/PersonajeServiceImplTest.java
@@ -0,0 +1,106 @@
+package com.marvel.microservicio.microservicio.service.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.marvel.microservicio.repository.BitacoraRepository;
+import com.marvel.microservicio.service.impl.PersonajeServiceImpl;
+
+/**
+ * Clase test de la clase PersonajeServiceImplTest
+ */
+@ExtendWith(MockitoExtension.class)
+public class PersonajeServiceImplTest {
+	
+	/**
+	 * Inyeccion
+	 */
+	@Mock
+	private BitacoraRepository bitacoraRepository;
+	
+	/**
+	 * Instancia
+	 */
+	@InjectMocks
+	private PersonajeServiceImpl personajeServiceImpl;
+	
+	/**
+	 * resultadoAll
+	 */
+	private Object resultadoAll;
+	
+	/**
+	 * resultadoOne
+	 */
+	private Object resultadoOne;
+	
+	/**
+	 * resultadoBitacora
+	 */
+	private Object resultadoBitacora;
+	
+	/**
+	 * noEsperado
+	 */
+	private Object noEsperado;
+	
+	/**
+	 * id
+	 */
+	private Long id = 1011334L;
+	
+	/**
+	 * Metodo setup ejecuciones previas a los Test
+	 */
+	@BeforeEach
+    public void setup(){
+		resultadoAll = personajeServiceImpl.getAllCharacters();
+		resultadoOne = personajeServiceImpl.getCharacterById(id);
+		resultadoBitacora = personajeServiceImpl.getBitacora();
+    }
+	
+	/**
+	 * Test del metodo getAllCharacters
+	 */
+	@DisplayName("JUnit test metodo getAllCharacters")
+	@Test
+	void testGetAllCharacters() {
+		final Object esperado = personajeServiceImpl.getAllCharacters();
+		Assertions.assertNotNull(esperado);
+		Assertions.assertEquals(resultadoAll, esperado);
+		Assertions.assertNotEquals(resultadoOne, esperado);
+		Assertions.assertNotSame(noEsperado, esperado);
+	}
+	
+	/**
+	 * Test del metodo getCharacterById
+	 */
+	@DisplayName("JUnit test metodo getCharacterById")
+	@Test
+	void testGetCharacterById() {
+		final Object esperado = personajeServiceImpl.getCharacterById(id);
+		Assertions.assertNotNull(esperado);
+		Assertions.assertEquals(resultadoOne, esperado);
+		Assertions.assertNotEquals(resultadoAll, esperado);
+		Assertions.assertNotSame(noEsperado, esperado);
+	}
+	
+	/**
+	 * Test del metodo getBitacora
+	 */
+	@DisplayName("JUnit test metodo getBitacora")
+	@Test
+	void testGetBitacora() {
+		final Object esperado = personajeServiceImpl.getBitacora();
+		Assertions.assertNotNull(esperado);
+		Assertions.assertEquals(resultadoBitacora, esperado);
+		Assertions.assertNotEquals(resultadoAll, esperado);
+		Assertions.assertNotSame(noEsperado, esperado);
+	}
+
+}
